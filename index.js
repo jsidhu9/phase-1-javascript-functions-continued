70148511fa9d0158882fa5af801fe4fa8183bce8
+ <+>
2022-03-15 21:21:11 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // code your solution here
-function saturdayFun(activity='roller-skate') {
-    return `This Saturday, I want to ${activity}!`
-  }
-function mondayWork(activity='go to the office') {
-    return `This Monday, I will ${activity}.`
+function saturdayFun (activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`
 }
-let wrapAdjective = function(style="*") {
-    return function(adjective="special") {
-      return `You are ${style}${adjective}${style}!`
-    }
+
+function mondayWork (activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective (style='*') {
+  return function (adjective='special') {
+    return `You are ${style}${adjective}${style}!`
   }
+  
+}
